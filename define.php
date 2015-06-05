ee153faa9fe0994f231bc403fbc3676ece49ad56
yuuya1023 <wish.on.the.shootingstar@gmail.com>
2015-06-05 14:59:14 +0900

    define修正

--- define.php
+++ define.php
@@ -3,13 +3,9 @@
 define( "TWITTER_API_KEY", "" );
 define( "TWITTER_API_KEY_SECRET", "" );
 
-define( "TWITTER_ACCES_TOKEN", "" );
-define( "TWITTER_ACCES_TOKEN_SECRET", "" );
-
-define('CALLBACK_URL', 'http://192.168.33.10/vagrant/src/app/twitter/authorize/callback.php');
+define( "CALLBACK_URL", "http://~~/src/app/twitter/authorize/callback.php" );
 
 define( "TUMBLR_API_KEY", "" );
-define( "TUMBLR_HOST", "" );
 
 define( "DATABASE_HOST", "" );
 define( "DATABASE_USER", "" );
