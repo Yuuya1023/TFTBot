52f239053249d39dc4e03b3a9236f067633ce3fa
yuuya1023 <wish.on.the.shootingstar@gmail.com>
2015-06-25 18:51:41 +0900

    カンマ区切りで検索できるように修正、あとエラー時にDM

--- src/app/search_twitter.php
+++ src/app/search_twitter.php
@@ -55,43 +55,67 @@ class SearchTwitter
 			$notice_user = $word_res["notice_user"];
 			$latest_tweet_id = $word_res["latest_tweet_id"];
 
-			// 検索
-			$search_result = $twitter_manager->search( $oauth_object, $word, 100, $latest_tweet_id );
-			$statuses = $search_result->statuses;
-			if ( $statuses > 0 ) {
-				$latest_id = null;
-				$current_date = date('Y年m月d日 H時i分');
-				$direct_message_text = "search word:\n" . $word . "\nat:\n" . $current_date . "\n";
-				// for ($i=0; $i < count($statuses); $i++) {
-				for ($i=count($statuses) - 1; $i >= 0; $i--) {
-					$streaming_obj = new StreamingObject();
-					$streaming_obj->initWithJson( $statuses[$i] );
+			$kown_tweet_id_buffer = array();
 
-					if ( !$streaming_obj->isRetweeted() ) {
-						// デバッグ
-						// $streaming_obj->displayTweet();
-						print_r("\n" . $streaming_obj->getId() );
+			// 検索文字列を,で分割
+			$word_list = explode( ",", $word );
+			foreach ( $word_list as $w ) {
+				// print_r("\n" . $w );
+				
+				// 検索
+				$search_result = $twitter_manager->search( $oauth_object, $w, 100, $latest_tweet_id );
+				$statuses = $search_result->statuses;
+				if ( $statuses > 0 ) {
+					$latest_id = null;
+					$current_date = date('Y年m月d日 H時i分');
+					$direct_message_text = "search word:\n" . $w . "\nat:\n" . $current_date . "\n";
+					// for ($i=0; $i < count($statuses); $i++) {
+					for ($i=count($statuses) - 1; $i >= 0; $i--) {
+						$streaming_obj = new StreamingObject();
+						$streaming_obj->initWithJson( $statuses[$i] );
 
-						// DMで送る文字列生成
-						// $direct_message_text .= $streaming_obj->getText() . "\n" . $streaming_obj->generateTweetLink() . "\n\n";	// 文字数制限解除されたら
-						// DM送信
-						$t = $direct_message_text . $streaming_obj->generateTweetLink();
-						$res = $twitter_manager->sendDirectMessage( $oauth_object, $notice_user, $t );
-						// print_r($res);
+						if ( !$streaming_obj->isRetweeted() && !in_array($streaming_obj->getId(), $kown_tweet_id_buffer) ) {
+							// デバッグ
+							// $streaming_obj->displayTweet();
+							// print_r("\n" . $streaming_obj->getId() );
+							// print_r("\n" . $streaming_obj->getScreenName() );
+							// print_r("\n" . $streaming_obj->generateTweetLink() );
+
+							// DMで送る文字列生成
+							// $direct_message_text .= $streaming_obj->getText() . "\n" . $streaming_obj->generateTweetLink() . "\n\n";	// 文字数制限解除されたら
+							// DM送信
+							$t = $direct_message_text . $streaming_obj->generateTweetLink();
+							$res = $twitter_manager->sendDirectMessage( $oauth_object, $notice_user, $t );
+							// print_r($res);
+							if ( array_key_exists( "errors", $res ) ) {
+								// エラー
+								$error_msg = "notice user :\n@" . $notice_user . "\n";
+								$errors = $res->errors;
+								foreach ($errors as $error) {
+									$error_msg .= $error->code . " " . $error->message . ", ";
+								}
+								$twitter_manager->sendDirectMessage( $oauth_object, "tyorokunai_man", mb_substr($error_msg, 0 , 130) );
+								return;
+							}
+							else {
+								// DM送信済みとしてリストに保存しておく
+								$kown_tweet_id_buffer[] = $streaming_obj->getId();
+							}
+						}
+
+						if ( $i === 0 ) {
+							$latest_id = $streaming_obj->getId();
+						}
 					}
+					// print_r($direct_message_text);
 
-					if ( $i === 0 ) {
-						$latest_id = $streaming_obj->getId();
-					}
+					// DM送信
+					// $res = $twitter_manager->sendDirectMessage( $oauth_object, $notice_user, $direct_message_text ); // 文字数制限解除されたら
+					// print_r($res);
+
+					// latest_id更新
+					DatabaseHelper::updateLatestTweetId( $database_manager, $latest_id, $word_id );
 				}
-				// print_r($direct_message_text);
-
-				// DM送信
-				// $res = $twitter_manager->sendDirectMessage( $oauth_object, $notice_user, $direct_message_text ); // 文字数制限解除されたら
-				// print_r($res);
-
-				// latest_id更新
-				DatabaseHelper::updateLatestTweetId( $database_manager, $latest_id, $word_id );
 			}
 		}
 	}
